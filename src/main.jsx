aeff3c16792e9a03adbaf35458680ca4f251f225
Yagnit <yagnitmahajan881@gmail.com>
2025-02-03 22:17:52 +0530

    Added main.jsx file

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Assuming you have an App component
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
